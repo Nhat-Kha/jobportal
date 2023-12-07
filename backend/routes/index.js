d03e8845bc9bed2a161fe534f1d4456b9e16f664
Kaio-Nguyen <nhatkha06299@gmail.com>
2023-12-07 14:04:19 +0700

    add new upload and fix  update index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,8 @@ const userRouter = require("./user");
 const jobRouter = require("./job");
 const applicationRouter = require("./applications");
 const ratingRouter = require("./rating");
+const uploadRouter = require("./upload");
+const downloadRouter = require("./download");
 
 /**
  *
@@ -14,6 +16,8 @@ const initRouter = (app) => {
   app.use("/api/job", jobRouter);
   app.use("/api/application", applicationRouter);
   app.use("/api/rating", ratingRouter);
+  app.use("/api/upload", uploadRouter);
+  app.use("/api/download", downloadRouter);
 };
 
 module.exports = initRouter;
