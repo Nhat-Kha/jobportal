811107205bd83df2f5a4db46e6e610223f1bda98
Kaio-Nguyen <nhatkha06299@gmail.com>
2023-12-08 13:35:01 +0700

    fix app.js, create component, page for web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Grid, makeStyles } from "@material-ui/core";
+
+import Welcome, { ErrorPage } from "./component/Welcome";
+import Navbar from "./component/Navbar";
+import Login from "./component/Login";
+import Logout from "./component/Logout";
+import Signup from "./component/Signup";
+import Home from "./component/Home";
+import Applications from "./component/Applications";
+import Profile from "./component/Profile";
+import CreateJobs from "./component/recruiter/CreateJobs";
+import MyJobs from "./component/recruiter/MyJobs";
+import JobApplications from "./component/recruiter/JobApplications";
+import AcceptedApplicants from "./component/recruiter/AcceptedApplicants";
+import RecruiterProfile from "./component/recruiter/Profile";
+import MessagePopup from "./lib/MessagePopup";
+import isAuth, { userType } from "./lib/isAuth";
+
+const useStyles = makeStyles((theme) => ({
+  body: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "98vh",
+    paddingTop: "64px",
+    boxSizing: "border-box",
+    width: "100%",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Grid container direction="column">
+        <Grid Item xs></Grid>
+        <Grid></Grid>
+      </Grid>
+      <MessagePopup></MessagePopup>
+    </BrowserRouter>
   );
 }
 
