e1176a6de05dd2ca8e6590b5cbe23dc9169a4cc2
Kaio-Nguyen <nhatkha06299@gmail.com>
2023-12-13 13:26:46 +0700

    create image/index, add image to index

---
+++ frontend/src/images/index.js
@@ -0,0 +1,6 @@
+import searchImage from "./hero.png";
+import jobImage from "./job.jpg";
+import officeImage from "./office.jpg";
+import errorImage from "./errorpage.webp";
+
+export { searchImage, jobImage, officeImage, errorImage };
