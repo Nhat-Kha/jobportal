775e0b3fff37ab5bbe57762e4b183e03c3220d0c
Kaio-Nguyen <nhatkha06299@gmail.com>
2023-12-16 18:51:58 +0700

    add new infor bar

---
+++ frontend/src/components/InfoBar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export default function InfoBar() {
+  return (
+    <div className=" bg-money text-center">
+      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+        <div className="relative">
+          <p className="ml-3 text-black font-medium">
+            <span className="mr-2">💸</span>
+            <span>
+              <strong> Welcome</strong> to <strong>my</strong> Website
+              available.{" "}
+              <Link to="/jobs" className="border-b-2 border-black ">
+                Find what you do, Work you like❤️
+              </Link>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
