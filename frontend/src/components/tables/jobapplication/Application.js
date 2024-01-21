cd5d77ef571d45ad362e5a9ad79a91461ee279ca
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-01-21 23:38:26 +0700

    fix UI profile recruiter

--- frontend/src/components/tables/jobapplication/Application.js
+++ frontend/src/components/tables/jobapplication/Application.js
@@ -87,10 +87,10 @@ const ApplicationTile = (props) => {
 
   const buttonSet = {
     applied: (
-      <>
+      <div className="w-[5.75rem] py-4 whitespace-nowrap text-sm text-gray-500 flex flex-col gap-2">
         <div>
           <button
-            className="w-full h-full flex items-center justify-center uppercase"
+            className="w-full h-full flex items-center justify-center uppercase font-semibold rounded-md"
             style={{
               background: colorSet["shortlisted"],
               color: "#ffffff",
@@ -102,7 +102,7 @@ const ApplicationTile = (props) => {
         </div>
         <div>
           <button
-            className="w-full h-full flex items-center justify-center uppercase"
+            className="w-full h-full flex items-center justify-center uppercase font-semibold rounded-md"
             style={{
               background: colorSet["shortlisted"],
               color: "#ffffff",
@@ -112,13 +112,13 @@ const ApplicationTile = (props) => {
             Reject
           </button>
         </div>
-      </>
+      </div>
     ),
     shortlisted: (
-      <>
+      <div className="w-[5.75rem] py-4 whitespace-nowrap text-sm text-gray-500 flex flex-col gap-2">
         <div>
           <button
-            className="w-full h-full flex items-center justify-center uppercase"
+            className="w-full h-full flex items-center justify-center uppercase font-semibold rounded-md"
             style={{
               background: colorSet["accepted"],
               color: "#ffffff",
@@ -130,7 +130,7 @@ const ApplicationTile = (props) => {
         </div>
         <div>
           <button
-            className="w-full h-full flex items-center justify-center uppercase"
+            className="w-full h-full flex items-center justify-center uppercase font-semibold rounded-md"
             style={{
               background: colorSet["rejected"],
               color: "#ffffff",
@@ -140,13 +140,13 @@ const ApplicationTile = (props) => {
             Reject
           </button>
         </div>
-      </>
+      </div>
     ),
     rejected: (
-      <>
+      <div className="w-[5.75rem] py-4 whitespace-nowrap text-sm text-gray-500 flex flex-col gap-2">
         <div>
           <button
-            className="w-full h-full flex items-center justify-center uppercase "
+            className="w-full h-full flex items-center justify-center uppercase font-semibold rounded-md"
             style={{
               background: colorSet["rejected"],
               color: "#ffffff",
@@ -155,13 +155,13 @@ const ApplicationTile = (props) => {
             Rejected
           </button>
         </div>
-      </>
+      </div>
     ),
     accepted: (
-      <>
+      <div className="w-[5.75rem] py-4 whitespace-nowrap text-sm text-gray-500 flex flex-col gap-2">
         <div item xs>
           <button
-            className="w-full h-full flex items-center justify-center uppercase "
+            className="w-full h-full flex items-center justify-center uppercase font-semibold rounded-md"
             style={{
               background: colorSet["accepted"],
               color: "#ffffff",
@@ -170,13 +170,13 @@ const ApplicationTile = (props) => {
             Accepted
           </button>
         </div>
-      </>
+      </div>
     ),
     cancelled: (
-      <>
+      <div className="w-[5.75rem] py-4 whitespace-nowrap text-sm text-gray-500 flex flex-col gap-2">
         <div>
           <button
-            className="w-full h-full flex items-center justify-center uppercase "
+            className="w-full h-full flex items-center justify-center uppercase font-semibold rounded-md"
             style={{
               background: colorSet["cancelled"],
               color: "#ffffff",
@@ -185,13 +185,13 @@ const ApplicationTile = (props) => {
             Cancelled
           </button>
         </div>
-      </>
+      </div>
     ),
     finished: (
-      <>
+      <div className="w-[5.75rem] py-4 whitespace-nowrap text-sm text-gray-500 flex flex-col gap-2">
         <button>
           <button
-            className="w-full h-full flex items-center justify-center uppercase "
+            className="w-full h-full flex items-center justify-center uppercase font-semibold rounded-md"
             style={{
               background: colorSet["finished"],
               color: "#ffffff",
@@ -200,17 +200,17 @@ const ApplicationTile = (props) => {
             Finished
           </button>
         </button>
-      </>
+      </div>
     ),
   };
 
   return (
-    <tr className="flex">
+    <tr className="flex items-center">
       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 flex-1 w-[30rem]">
         <img
           src={`${application.jobApplicant.profile}`}
           alt={`${application.jobApplicant.name}'s profile`}
-          className="w-[10rem] h-[10rem]"
+          className="w-[10rem] h-[10rem] rounded-xl"
         />
       </td>
       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 flex-1 w-[30rem]">
@@ -227,28 +227,53 @@ const ApplicationTile = (props) => {
             readOnly
           />
         </div>
-        <div>Applied On: {appliedOn.toLocaleDateString()}</div>
+        <div>
+          Applied On:{" "}
+          <span className="font-semibold">
+            {appliedOn.toLocaleDateString()}
+          </span>
+        </div>
         <div>
           Education:{" "}
-          {application.jobApplicant.education
-            .map((edu) => {
-              return `${edu.institutionName} (${edu.startYear}-${
-                edu.endYear ? edu.endYear : "Ongoing"
-              })`;
-            })
-            .join(", ")}
+          <span className="font-semibold">
+            {application.jobApplicant.education
+              .map((edu) => {
+                return `${edu.institutionName} (${edu.startYear}-${
+                  edu.endYear ? edu.endYear : "Ongoing"
+                })`;
+              })
+              .join(", ")}
+          </span>
+        </div>
+        <div className="mt-2">
+          <td className="text-bold">Skills:</td>
+          <td className="text-right">
+            <div className="flex flex-row-reverse gap-1">
+              {application.jobApplicant.skills.map((tag, index) => (
+                <div
+                  key={index}
+                  class="relative grid select-none items-center whitespace-nowrap rounded-lg 
+                          bg-gray-900 py-1.5 px-3 font-sans text-xs font-bold uppercase text-white"
+                >
+                  <span class="">{tag}</span>
+                </div>
+              ))}
+            </div>
+          </td>
         </div>
       </td>
-      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+      <td className="px-6 py-4 w-[20%] whitespace-nowrap text-sm text-gray-500">
         <div>
           <button
-            className="w-full h-full flex items-center justify-center uppercase bg-gray-400"
+            className="w-full h-full flex items-center justify-center uppercase bg-gray-400 opacity-80 text-white font-semibold rounded-md"
             onClick={() => getResume()}
           >
             Download Resume
           </button>
         </div>
-        <div className="flex container">{buttonSet[application.status]}</div>
+        <div className="flex container rounded-md">
+          {buttonSet[application.status]}
+        </div>
       </td>
       <Modal>
         <div className="p-[20px] outline-none flex flex-col justify-center min-w-[30%] items-center">
