e7e9d48f3e4186a8ffbb54fa6990482398671bb5
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-03-06 12:20:14 +0700

    update and done feature delete job for admin and recruiter

--- backend/controller/job.js
+++ backend/controller/job.js
@@ -529,30 +529,29 @@ const getApplications = async (req, res) => {
 
 const deleteJob = async (req, res) => {
   const user = req.user;
-  if (user.type != "recruiter") {
-    res.status(401).json({
+  if (user.type !== "recruiter" && user.type !== "admin") {
+    return res.status(401).json({
       message: "You don't have permissions to delete the job",
     });
-    return;
   }
-  Job.findOneAndDelete({
-    _id: req.params.id,
-    userId: user.id,
-  })
-    .then((job) => {
-      if (job === null) {
-        res.status(401).json({
-          message: "You don't have permissions to delete the job",
-        });
-        return;
-      }
-      res.json({
-        message: "Job deleted successfully",
-      });
-    })
-    .catch((err) => {
-      res.status(400).json(err);
+
+  try {
+    const job = await Job.findOneAndDelete({
+      _id: req.params.id,
     });
+    if (!job) {
+      return res.status(404).json({
+        message: "Job not found",
+      });
+    }
+    res.json({
+      message: "Job deleted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
 
 module.exports = {
