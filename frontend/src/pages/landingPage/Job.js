0cf2b986776abd3cda87a6051792531803c88622
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-03-10 13:32:55 +0700

    add right similar job post

--- frontend/src/pages/landingPage/Job.js
+++ frontend/src/pages/landingPage/Job.js
@@ -12,6 +12,7 @@ export default function Job(props) {
   const setPopup = useContext(SetPopupContext);
   const { id } = useParams();
   const [job, setJob] = useState();
+  const [allJob, setAllJob] = useState();
   const [hasAcceptedJob, setHasAcceptedJob] = useState(false);
   const [open, setOpen] = useState(false);
   const handleClose = () => {
@@ -69,6 +70,10 @@ export default function Job(props) {
       });
   };
 
+  const handleClickJob = () => {
+    history.go(0);
+  };
+
   useEffect(() => {
     const checkAcceptedJob = async () => {
       try {
@@ -101,23 +106,129 @@ export default function Job(props) {
       });
   }, []);
 
+  useEffect(() => {
+    const all = apiList.jobs;
+    axios.get(all).then((response) => {
+      setAllJob(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
+  function calculateDays(date) {
+    let daysAgo = Math.floor((new Date() - date) / (1000 * 3600 * 24));
+
+    if (daysAgo < 1) {
+      return "Today";
+    } else if (daysAgo < 2) {
+      return daysAgo + " day ago";
+    } else if (daysAgo < 7) {
+      return daysAgo + " days ago";
+    } else if (daysAgo < 14) {
+      return "1 week ago";
+    } else if (daysAgo < 30) {
+      return Math.floor(daysAgo / 7) + " weeks ago";
+    } else if (daysAgo < 60) {
+      return "1 month ago";
+    } else {
+      return Math.floor(daysAgo / 30) + " months ago";
+    }
+  }
+
   return (
     <>
-      <div className="lg:w-6/12 w-11/12 mx-auto md:mt-20 mt-10 pb-10">
-        <JobAd about={job} />
-        <div className="text-center mx-auto mt-12 mb-10">
-          {userType() === "applicant" ? (
-            <Link
-              className={`hover:opacity-80 ease-out duration-300 flex cursor-pointer items-center font-semibold 
+      <div className="flex">
+        <div className="lg:w-6/12 w-11/12 h-full ml-44 mr-20 md:mt-20 mt-10 pb-10">
+          <JobAd about={job} />
+          <div className="text-center mx-auto mt-12 mb-10">
+            {userType() === "applicant" ? (
+              <Link
+                className={`hover:opacity-80 ease-out duration-300 flex cursor-pointer items-center font-semibold 
                 text-md justify-center px-8 py-3 bg-primary rounded-xl text-black ${
                   hasAcceptedJob ? "opacity-50 cursor-not-allowed" : ""
                 }`}
-              onClick={() => handleApply()}
-              title={hasAcceptedJob ? "You already have an accepted job" : ""}
-            >
-              {hasAcceptedJob ? "Job accepted!" : "Apply"}
-            </Link>
-          ) : null}
+                onClick={() => handleApply()}
+                title={hasAcceptedJob ? "You already have an accepted job" : ""}
+              >
+                {hasAcceptedJob ? "Job accepted!" : "Apply"}
+              </Link>
+            ) : null}
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-full md:w-1/3 2xl:w-2/4 md:mt-20 mt-10 pb-10">
+          <p className="text-gray-500 font-semibold">Similar Job Post</p>
+
+          <div className="w-full flex flex-wrap gap-4">
+            {allJob?.slice(0, 6).map(
+              (job, index) =>
+                job._id !== id && (
+                  <Link
+                    to={`/jobs/${job._id}`}
+                    key={index}
+                    onClick={handleClickJob}
+                  >
+                    <div
+                      className="w-full md:w-[16rem] 2xl:w-[18rem] h-[16rem] md:h-[18rem] bg-white flex flex-col justify-between shadow-lg 
+                      rounded-md px-3 py-5 "
+                    >
+                      <div className="flex gap-3">
+                        <img
+                          src={job?.recruiter.profile}
+                          alt={job?.recruiter.name}
+                          className="w-14 h-14"
+                        />
+
+                        <div className="">
+                          <p className="text-lg font-semibold truncate">
+                            {job.title}
+                          </p>
+                          <span className="flex gap-2 items-center">
+                            {job.location}
+                          </span>
+                        </div>
+                      </div>
+
+                      <div className="py-3">
+                        <p className="text-sm">
+                          {job.description ? (
+                            <div
+                              className="text-base"
+                              dangerouslySetInnerHTML={{
+                                __html: job.description,
+                              }}
+                            ></div>
+                          ) : (
+                            <div className="text-base">No description yet</div>
+                          )}
+                        </p>
+                      </div>
+
+                      <div className="flex items-center justify-between">
+                        <p className="bg-[#1d4fd826] text-[#1d4fd8] py-0.5 px-1.5 rounded font-semibold text-sm">
+                          {job.jobType}
+                        </p>
+                        <span className="text-gray-500 text-sm">
+                          {calculateDays(new Date(job.dateOfPosting))}{" "}
+                        </span>
+                      </div>
+                      <div>
+                        <div className="flex flex-row gap-1">
+                          {job.skillsets.map((tag, index) => (
+                            <div
+                              key={index}
+                              className="relative grid select-none items-center whitespace-nowrap rounded-lg 
+                          bg-gray-900 py-1.5 px-3 font-sans text-xs font-bold uppercase text-white"
+                            >
+                              <span className="">{tag}</span>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                )
+            )}
+          </div>
         </div>
       </div>
 
