02a89c53ae4cb11ed85dba93ce72ec8c766f4375
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-04-05 14:18:15 +0700

    update navbar of page blog

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,149 +1,308 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "assets/images/logo.png";
 import LinkedIn from "assets/LinkedIn.png";
 import instagram from "assets/Instagram.webp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBlog } from "@fortawesome/free-solid-svg-icons";
+import InputField from "./InputField";
 
 export default function Footer() {
+  const linkUrl = useLocation();
   return (
     <div className="bg-[#FFF5EC] text-whblackite text-left">
-      <div className="w-10/12 mx-auto pt-20 pb-8 flex flex-col md:flex-row sm:flex-row space-y-2 justify-start">
-        <div className="w-full sm:w-2/5 md:w-3/5 pr-6 flex flex-col space-y-2 pb-6 cursor-default">
-          <Link className="flex pt-1" to="/">
-            <img className="h-12 pb-2 " src={logo} alt="logo" />
-            <h1 className="md:pl-2 pl-2 text-4xl text-black font-medium hover:opacity-60">
-              JobPortal
-            </h1>
-          </Link>
-          <p className="md:w-80 w-full text-lg font-medium -mt-2 ">
-            Quality over quantity.
-          </p>
+      {linkUrl.pathname !== "/blog" ? (
+        <div className="w-10/12 mx-auto pt-20 pb-8 flex flex-col md:flex-row sm:flex-row space-y-2 justify-start">
+          <div className="w-full sm:w-2/5 md:w-3/5 pr-6 flex flex-col space-y-2 pb-6 cursor-default">
+            <Link className="flex pt-1" to="/">
+              <img className="h-12 pb-2 " src={logo} alt="logo" />
+              <h1 className="md:pl-2 pl-2 text-4xl text-black font-medium hover:opacity-60">
+                JobPortal
+              </h1>
+            </Link>
+            <p className="md:w-80 w-full text-lg font-medium -mt-2 ">
+              Quality over quantity.
+            </p>
 
-          <a
-            className="w-[220px] text-lg font-medium border-b-2 border-black hover:opacity-60"
-            href="mailto:nhatkha06299@gmail.com"
-          >
-            nhatkha06299@gmail.com
-          </a>
-          <span className="font-light">
-            Copyright © 2023 <span className="font-normal"> Nhat kha</span>{" "}
-            Design.
-          </span>
+            <a
+              className="w-[220px] text-lg font-medium border-b-2 border-black hover:opacity-60"
+              href="mailto:nhatkha06299@gmail.com"
+            >
+              nhatkha06299@gmail.com
+            </a>
+            <span className="font-light">
+              Copyright © 2023 <span className="font-normal"> Nhat kha</span>{" "}
+              Design.
+            </span>
+          </div>
+          <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6 cursor-default">
+            <h1 className="text-2xl font-semibold">Welcome</h1>
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/for-applicant"
+            >
+              How it works
+            </Link>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/jobs"
+            >
+              Find jobs
+            </Link>
+
+            <Link
+              className="w-40 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/companies"
+            >
+              Find companies
+            </Link>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/sign-in"
+            >
+              Sign in
+            </Link>
+
+            <Link
+              className="w-40 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/sign-up/new-applicant"
+            >
+              Sign up
+            </Link>
+          </div>
+
+          <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6">
+            <h1 className="text-2xl font-semibold">Companies</h1>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/for-recruiter"
+            >
+              How it works
+            </Link>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/create-new-job"
+            >
+              Create job
+            </Link>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/sign-in"
+            >
+              Sign in
+            </Link>
+
+            <Link
+              className="w-40 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/sign-up/new-recruiter"
+            >
+              Sign up
+            </Link>
+          </div>
+
+          <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6">
+            <h1 className="text-2xl font-semibold">JobPortal</h1>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/about"
+            >
+              About us
+            </Link>
+
+            <Link
+              className="opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/privacy-policy"
+            >
+              Privacy policy
+            </Link>
+
+            <Link
+              className="opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/cookie-policy"
+            >
+              Cookie policy
+            </Link>
+          </div>
+
+          <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6">
+            <h1 className="text-2xl font-semibold">Follow us</h1>
+            <a
+              className="w-28 font-semibold text-lg"
+              href="https://www.instagram.com/kha_martin/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={instagram} alt="Instagram logo" className="w-12 h-12" />
+            </a>
+            <a
+              className="w-28 opacity-80 hover:opacity-100 font-semibold text-lg"
+              href="https://github.com/Nhat-Kha"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={LinkedIn} alt="LinkedIn logo" className="w-12 h-12" />
+            </a>
+          </div>
         </div>
-        <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6 cursor-default">
-          <h1 className="text-2xl font-semibold">Welcome</h1>
-          <Link
-            className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/for-applicant"
-          >
-            How it works
-          </Link>
+      ) : (
+        <div className="w-10/12 mx-auto pt-20 pb-8 flex flex-col md:flex-row sm:flex-row space-y-2 justify-start">
+          <div className="w-full sm:w-2/5 md:w-3/5 pr-6 flex flex-col space-y-2 pb-6 cursor-default">
+            <Link className="flex pt-1" to="/">
+              <img className="h-12 pb-2 " src={logo} alt="logo" />
+              <h1 className="md:pl-2 pl-2 text-4xl text-black font-medium hover:opacity-60">
+                JobPortal
+              </h1>
+              <FontAwesomeIcon icon={faBlog} />
+            </Link>
+            <div className="w-full md:w-2/4 lg:w-1/3 h-16 flex items-center justify-center md:justify-start ">
+              <input
+                placeholder="Email Address"
+                className="p-4 rounded outline-none bg-gray-50 focus:bg-white transition duration-200"
+              />
+            </div>
+            <div>
+              <button
+                className="text-center transform hover:bg-yellow-400 transition duration-200
+              cursor-pointer font-bold text-md px-8 py-3 bg-yellow-300 rounded-xl text-black"
+              >
+                Send mail
+              </button>
+            </div>
+            <span className="break-all w-3/4">
+              By providing your email address, you agree to the{" "}
+              <strong>Terms of Service</strong> and{" "}
+              <strong>Privacy Policy</strong> of JobPortal regarding your
+              privacy information.
+            </span>
+            <div className="flex gap-4">
+              <div className="w-10 h-10">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+                  <path
+                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 
+                    0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3
+                    4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 
+                    .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 
+                    8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 
+                    0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 
+                    27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 
+                    2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 
+                    67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 
+                    4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9
+                    2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 
+                    .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 
+                    .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 
+                    0-6.2-1.4-2.3-4-3.3-5.6-2z"
+                  />
+                </svg>
+              </div>
+              <div className="w-10 h-10">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6 cursor-default">
+            <h1 className="text-2xl font-semibold">Welcome</h1>
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/for-applicant"
+            >
+              How it works
+            </Link>
 
-          <Link
-            className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/jobs"
-          >
-            Find jobs
-          </Link>
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/jobs"
+            >
+              Find jobs
+            </Link>
 
-          <Link
-            className="w-40 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/companies"
-          >
-            Find companies
-          </Link>
+            <Link
+              className="w-40 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/companies"
+            >
+              Find companies
+            </Link>
 
-          <Link
-            className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/sign-in"
-          >
-            Sign in
-          </Link>
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/sign-in"
+            >
+              Sign in
+            </Link>
 
-          <Link
-            className="w-40 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/sign-up/new-applicant"
-          >
-            Sign up
-          </Link>
+            <Link
+              className="w-40 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/sign-up/new-applicant"
+            >
+              Sign up
+            </Link>
+          </div>
+
+          <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6">
+            <h1 className="text-2xl font-semibold">Companies</h1>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/for-recruiter"
+            >
+              How it works
+            </Link>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/create-new-job"
+            >
+              Create job
+            </Link>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/sign-in"
+            >
+              Sign in
+            </Link>
+
+            <Link
+              className="w-40 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/sign-up/new-recruiter"
+            >
+              Sign up
+            </Link>
+          </div>
+
+          <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6">
+            <h1 className="text-2xl font-semibold">JobPortal</h1>
+
+            <Link
+              className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/about"
+            >
+              About us
+            </Link>
+
+            <Link
+              className="opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/privacy-policy"
+            >
+              Privacy policy
+            </Link>
+
+            <Link
+              className="opacity-60 hover:opacity-100 font-semibold text-lg"
+              to="/cookie-policy"
+            >
+              Cookie policy
+            </Link>
+          </div>
         </div>
-
-        <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6">
-          <h1 className="text-2xl font-semibold">Companies</h1>
-
-          <Link
-            className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/for-recruiter"
-          >
-            How it works
-          </Link>
-
-          <Link
-            className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/create-new-job"
-          >
-            Create job
-          </Link>
-
-          <Link
-            className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/sign-in"
-          >
-            Sign in
-          </Link>
-
-          <Link
-            className="w-40 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/sign-up/new-recruiter"
-          >
-            Sign up
-          </Link>
-        </div>
-
-        <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6">
-          <h1 className="text-2xl font-semibold">JobPortal</h1>
-
-          <Link
-            className="w-28 opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/about"
-          >
-            About us
-          </Link>
-
-          <Link
-            className="opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/privacy-policy"
-          >
-            Privacy policy
-          </Link>
-
-          <Link
-            className="opacity-60 hover:opacity-100 font-semibold text-lg"
-            to="/cookie-policy"
-          >
-            Cookie policy
-          </Link>
-        </div>
-
-        <div className="w-full sm:w-1/5 md:w-2/5 flex flex-col space-y-2 pb-6">
-          <h1 className="text-2xl font-semibold">Follow us</h1>
-          <a
-            className="w-28 font-semibold text-lg"
-            href="https://www.instagram.com/kha_martin/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={instagram} alt="Instagram logo" className="w-12 h-12" />
-          </a>
-          <a
-            className="w-28 opacity-80 hover:opacity-100 font-semibold text-lg"
-            href="https://github.com/Nhat-Kha"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={LinkedIn} alt="LinkedIn logo" className="w-12 h-12" />
-          </a>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,58 @@
 import { Disclosure } from "@headlessui/react";
 import logo from "assets/images/logo.png";
 import HowIt from "./HowIt";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import MobileMenu from "./MobileMenu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faBlog, faPlus } from "@fortawesome/free-solid-svg-icons";
+import news from "data/authors-table-data";
 import ProfileMenu from "./ProfileMenu";
 import { userType } from "libs/isAuth";
 import isAuth from "libs/isAuth";
+import Blog from "./blog/Blog";
 
 export default function Navbar() {
+  const linkUrl = useLocation();
+
+  console.log(linkUrl);
+
   return (
     <Disclosure as="nav" className="bg-[#FFF5EC]  w-full">
-      {({ open }) => (
-        <>
-          <div className="flex justify-between h-24 py-6 md:w-10/12 w-11/12 mx-auto">
+      <>
+        <div className="flex justify-between h-24 py-6 md:w-10/12 w-11/12 mx-auto">
+          {linkUrl.pathname === "/blog" ||
+          linkUrl.pathname === "/blog/news" ||
+          linkUrl.pathname === "/blog/programming-language" ? (
+            <div className="flex">
+              <Link className="flex pt-1" to="/blog">
+                <img className="md:pl-5 pl-2" src={logo} alt="logo" />
+                <h1 className="md:pl-2 pl-2 text-2xl sm:text-2xl md:text-3xl text-[#F2994A] font-medium hover:opacity-60">
+                  JobPortal
+                </h1>
+                <FontAwesomeIcon icon={faBlog} />
+              </Link>
+              <div className="flex pt-0.5 pl-8 ">
+                <Link
+                  className="lg:block hidden text-[#333333] text-lg font-semibold pl-3 pr-6 py-2 hover:opacity-60"
+                  to="/"
+                >
+                  Home
+                </Link>
+                <Link
+                  className="lg:block hidden text-[#333333] text-lg font-semibold pl-3 pr-6 py-2 hover:opacity-60"
+                  to="/blog/news"
+                >
+                  News
+                </Link>
+                <Link
+                  className="lg:block hidden text-[#333333] text-lg font-semibold pl-3 pr-6 py-2 hover:opacity-60"
+                  to="/blog/programming-language"
+                >
+                  Programming Language
+                </Link>
+              </div>
+            </div>
+          ) : (
             <div className="flex">
               <Link className="flex pt-1" to="/">
                 <img className="md:pl-5 pl-2" src={logo} alt="logo" />
@@ -23,7 +61,7 @@ export default function Navbar() {
                 </h1>
               </Link>
 
-              <div className="flex pt-0.5 pl-8 ">
+              <div className="flex pt-0.5 pl-8">
                 <HowIt />
                 <Link
                   className="lg:block hidden text-[#333333] text-lg font-semibold pl-3 pr-6 py-2 hover:opacity-60"
@@ -45,47 +83,54 @@ export default function Navbar() {
                 >
                   Leaderboard
                 </Link>
+                <Blog />
               </div>
             </div>
+          )}
 
-            <div className="flex">
-              <MobileMenu />
-              {isAuth() ? (
-                <>
-                  {userType() === "recruiter" ? (
+          <div className="flex">
+            {linkUrl.pathname === "/blog" ||
+            linkUrl.pathname === "/blog/news" ||
+            linkUrl.pathname === "/blog/programming-language" ? null : (
+              <>
+                <MobileMenu />
+                {isAuth() ? (
+                  <>
+                    {userType() === "recruiter" ? (
+                      <Link
+                        to="/create-new-job"
+                        className="hidden sm:flex md:flex lg:flex xl:flex 2xl:flex  hover:opacity-80 cursor-pointer items-center font-semibold text-sm justify-center px-6 bg-black rounded-lg mr-8 text-white"
+                      >
+                        <FontAwesomeIcon icon={faPlus} className="mr-3" />
+                        Create new job
+                      </Link>
+                    ) : (
+                      ""
+                    )}
+
+                    <ProfileMenu type={userType} />
+                  </>
+                ) : (
+                  <>
                     <Link
-                      to="/create-new-job"
-                      className="hidden sm:flex md:flex lg:flex xl:flex 2xl:flex  hover:opacity-80 cursor-pointer items-center font-semibold text-sm justify-center px-6 bg-black rounded-lg mr-8 text-white"
+                      className="lg:block hidden text-black text-lg font-semibold pr-6 py-2 hover:opacity-60"
+                      to="/sign-in"
                     >
-                      <FontAwesomeIcon icon={faPlus} className="mr-3" />
-                      Create new job
+                      Sign in
                     </Link>
-                  ) : (
-                    ""
-                  )}
-
-                  <ProfileMenu type={userType} />
-                </>
-              ) : (
-                <>
-                  <Link
-                    className="lg:block hidden text-black text-lg font-semibold pr-6 py-2 hover:opacity-60"
-                    to="/sign-in"
-                  >
-                    Sign in
-                  </Link>
-                  <Link
-                    className="lg:block hidden text-center transform ease-in duration-100 hover:-translate-y-1 hover:shadow-lg w-32 justify-center px-8 py-2 mb-1 bg-[#F2994A] text-white rounded-full text-lg font-semibold"
-                    to="/sign-up"
-                  >
-                    Sign up
-                  </Link>
-                </>
-              )}
-            </div>
+                    <Link
+                      className="lg:block hidden text-center transform ease-in duration-100 hover:-translate-y-1 hover:shadow-lg w-32 justify-center px-8 py-2 mb-1 bg-[#F2994A] text-white rounded-full text-lg font-semibold"
+                      to="/sign-up"
+                    >
+                      Sign up
+                    </Link>
+                  </>
+                )}
+              </>
+            )}
           </div>
-        </>
-      )}
+        </div>
+      </>
     </Disclosure>
   );
 }
