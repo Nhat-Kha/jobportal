b263252d53d2e30b20221122ca35cf79d1cfb773
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-04-07 15:32:56 +0700

    update data blog fake

--- frontend/src/data/authors-table-data.js
+++ frontend/src/data/authors-table-data.js
@@ -1,7 +1,8 @@
 import ide from "../assets/ide.jpeg";
 import coder from "../assets/coder-programmer-developer.png";
 import html from "../assets/html.jpg";
-// import coder from "../assets/coder-programmer-developer.png";
+import coder1 from "../assets/CODER-768x768.png";
+import blog4 from "assets/blogs/blog-4.png";
 
 export const news = [
   {
@@ -12,14 +13,74 @@ export const news = [
 
     Below is the answer, let's explore with Hybrid Technologies:`,
     tags: ["IT"],
+    dateUpload: "hybrid-technologies",
+    about: [
+      `1. Coder - 
+      <img src="https://jobs.hybrid-technologies.vn/wp-content/uploads/2019/01/CODER.png" alt="" width="300" height="300">
+      Nếu bạn có 1 vấn đề rõ ràng và 1 giải pháp rõ ràng được mô tả bằng ngôn ngữ tự nhiên hoặc giải mã, bạn cần 1 <strong>Coder</strong> để chuyển chúng sang ngôn ngữ của máy tính (Chỉ code).
+      <br/>
+      <br/>
+      <strong>Coder</strong> – (hay thợ code), khi được giao việc code một chương trình nào đó thường cần phải kèm theo một bản mô tả chi tiết chương trình, rất thạo việc viết mã, viết nhanh và làm cho chương trình chạy đúng như mô tả, nhưng hầu hết trường hợp là sản phẩm không gọn gàng lắm nếu nhìn vô code.
+      <br/>
+      <br/>
+      Bất cứ ai khi viết code thường được người không rành công nghệ gọi là một coder. Tuy nhiên, thuật ngữ này có thể xem xét và sử dụng cho những ai sống bằng viết chương trình ứng dụng mỗi ngày. Coder thường được sử dụng cho những programmer có ít kinh nghiệm và được đào tạo ít nhất. Họ thường không có đủ kiến thức về các thuật toán như programmer hay developer, cũng như thường là cách gọi cho người mới bắt đầu vào nghề, và chỉ biết một ngôn ngữ lập trình duy nhất như là ngôn ngữ C, Java, PHP,… Coder thường được giao các công việc viết các đoạn mã đơn giản do các developer phân công. Trong một số trường hợp, coder còn được thay thế bằng “Junior Programmer” hoặc “Junior Developer”.`,
+      `2. Programmer - 
+      <img src="https://jobs.hybrid-technologies.vn/wp-content/uploads/2019/01/PROGRAMMER.png" alt="" width="300" height="300">
+
+      Nếu bạn có 1 vấn đề rõ ràng nhưng không có giải pháp rõ ràng, bạn cần một <strong>Programer</strong> để giải quyết vấn đề, sau đó code. <strong>(Giải quyết vấn đề + code)</strong>.
+      <br/>
+      <br/>
+      <strong>Programmer</strong> là người có chuyên môn hơn một chút. Họ có thể tạo ra phần mềm máy tính ở bất kỳ ngôn ngữ lập trình máy tính cơ bản nào, như Java, Python, Lisp,… Programmer được cho là vượt xa Coder, họ có thể chỉ chuyên môn trong một lĩnh vực hay thậm chí là viết hướng dẫn cho nhiều loại hệ thống khác nhau.
+      <br/>
+      <br/>
+      Programmer cũng am hiểu khá tốt về thuật toán. Họ cũng khá giống với Developer nhưng khác ở chỗ là Programmer chủ yếu chỉ triển khai hệ thống, trong khi Developer có thể thiết kế hoặc xây dựng một cấu trúc dữ liệu tốt trong phần mềm. Ngoài ra, Programmer sẽ quan tâm nhiều hơn vào chi tiết.`,
+      `3. Developer - 
+      <img src="https://jobs.hybrid-technologies.vn/wp-content/uploads/2019/01/DEVELOPER.png" alt="" width="300" height="300">
+
+      Nếu bạn cảm thấy mình có vấn đề cần giải quyết nhưng không thể định nghĩa rõ ràng được, bạn cần 1 <strong>Developer</strong> để giúp bạn tìm ra vấn đề trước, sau đó xử lý nó. <strong>(Phân tích + Giải quyết Vấn Đề + Code)</strong>.
+      <br/>
+      <br/>
+      Developer – Có tầm nhìn khái quát vấn đề, có khả năng sử dụng nhiều hệ thống khác nhau, nhiều ngôn ngữ lập trình, phối hợp chúng với nhau để giải quyết vấn đề. Những người này thường không chỉ làm việc chỉ với code mà cả với con người, thường là giao tiếp tốt, tiếp nhận vấn đề, phân tích và xây dựng giải pháp, rồi anh ta sẽ đưa ra thiết kế của các chương trình, cũng như cung cấp các mô tả chi tiết cho programmer và coder.
+      <br/>
+      <br/>
+      Vì vậy chúng ta có thể đúc kết được cơ sở để phân biệt các thuật ngữ này là dựa vào kĩ năng như am hiểu về công nghệ, kĩ thuật, thiết kế, về thuật toán, kĩ năng viết code…, dựa trên kinh nghiệm code thực tế (có thể quy bằng số năm như 1 năm hay 2 năm, hoặc bằng các ứng dụng đã tạo ra…), và cũng có thể dựa trên vị trí công việc của mỗi người tại một công ty.`,
+    ],
   },
   {
     id: 2,
     title: "The 12 Best IDEs for Programming",
+    dateUpload: "Franklin Okeke - techrepublic",
     img: ide,
     description:
       "IDEs are essential tools for software development. Here is a list of the top IDEs for programming.",
     tags: ["IDEs"],
+    about: [
+      `Best IDEs for programmers`,
+      `1. Visual Studio - 
+      The Visual Studio IDE is a Microsoft-powered integrated development interface developed to help software developers with web developments. The IDE uses artificial intelligence features to learn from the edit programmer’s make to their codes, making it easy for it to complete lines of code automatically.
+      
+      One of the top features many developers have come to like about Visual Studio is that it aids collaborative development between teams in live development. This feature is very crucial, especially during the debugging process. The IDE also allows users to share servers, comments and terminals.
+      
+      Visual Studio has the capability to support mobile app, web and game development. It also supports Python language, Node.js, ASP.NET and Azure. With Visual Studio, developers can easily create a development environment in the cloud.`,
+      `2. IntelliJ IDEA - 
+      IntelliJ Idea has been around for years and has served as one of the best IDEs for Java programming. The IntelliJ Idea UI is designed in a sleek way that makes coding appealing to many Java developers.
+      
+      With this IDE, code can get indexed, providing relevant suggestions to help complete code lines. It also takes this suggestive coding further by automating several tasks that may be repetitive.
+      
+      Apart from supporting web, enterprise, and mobile Java programming, it is also a good option for JavaScript, SQL and JPQL programming`,
+      `3. Xcode - 
+      Xcode might be the best IDE tool for Apple product developers. The tool supports iOS app development with its numerous iOS tools. The IDE supports programming languages such as Swift, C++ and Object-C.
+      
+      With XCode, developers can easily manage their software development workflow with quality code suggestions from the interface.`,
+      `4. Android Studio - 
+      The Android Studio is one of the best IDEs for Android app development. This IDE supports Kotlin and Java programming languages.
+      
+      Some important features users can get from the Android Studio are push alerts, camera integrations and other mobile technology features. Developers can also create variants and different APKs with the help of this flexible IDE, which also offers extended template support for Google Services.`,
+      `5. AWS Cloud9 IDE - 
+      The AWS Cloud9 IDE is packed with a terminal, a debugger and a code editor, and it supports popular programming languages such as Python and PHP.
+      
+      With Cloud9 IDE, software developers can work on their projects from almost anywhere in the globe as long as they have a computer that is connected to the internet, because it is cloud-based. Developers may create serverless applications using Cloud9 and easily collaborate with different teams in different development environments.`,
+    ],
   },
   {
     id: 3,
@@ -28,13 +89,16 @@ export const news = [
     description:
       "In HTML, there are various types of elements, each serving different purposes and having different attributes. Some elements are used to represent an entire document, while others describe smaller components. So, how do HTML tags function, and which types of tags are commonly used in programming tasks?",
     tags: ["html"],
+    about: [""],
   },
   {
     id: 4,
-    title: "The difference between CODER, PROGRAMMER, and DEVELOPER?",
-    img: "/assets/coder-programmer-developer.png",
-    description: "test",
-    tags: ["tags"],
+    title: "Your logo is copied",
+    dateUpload: "Ferdinand Vogler - ferdinandvogler",
+    img: blog4,
+    description: "Admit it, everything has been done before.",
+    tags: ["Graphic Design", "Branding"],
+    about: [""],
   },
 ];
 
