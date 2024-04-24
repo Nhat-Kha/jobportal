a354b42ce71274ec15ca71c9218288e47d81083d
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-04-24 21:44:09 +0700

    change apilist

--- frontend/src/libs/apiList.js
+++ frontend/src/libs/apiList.js
@@ -1,5 +1,6 @@
-export const server = "https://jobportal-6qtv.onrender.com/api";
-// "http://localhost:5000/api";
+export const server =
+  // "https://jobportal-6qtv.onrender.com/api";
+  "http://localhost:5000/api";
 
 const apiList = {
   login: `${server}/auth/login`,
